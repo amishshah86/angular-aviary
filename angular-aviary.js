9e4d81c657bdc4fc5af9ec3f59ea6c74c4ead799
Amish Shah <amishshah86@gmail.com>
2016-02-16 18:34:50 -0800

    reversed debug changes

--- angular-aviary.js
+++ angular-aviary.js
@@ -59,13 +59,11 @@
 
           function onSaveButtonClickedCb(imageID) {
             // User onSaveButtonClicked callback
-            console.log('Save Button Clicked: ', imageID);
             (scope.onSaveButtonClicked || angular.noop)({id: imageID});
           }
 
           function onSaveCb(imageID, newURL) {
             // User onSave callback
-            console.log('Save Successful: ', imageID);
             (scope.onSave || angular.noop)({
               id: imageID,
               newURL: newURL
